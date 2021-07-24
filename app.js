6059957d9f6b81314821d1cc525ad30b3a9bb626
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2021-07-25 00:38:22 +0500

    Updated

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 //Importing Pupprteer
-const puppeteer=require('puppeteer');
+const puppeteer = require("puppeteer");
+// This is where we'll put the code to get around the tests.
+const preparePageForTests = async (page) => {
+  // Pass the User-Agent Test.
+  const userAgent =
+    "Mozilla/5.0 (X11; Linux x86_64)" +
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36";
+  await page.setUserAgent(userAgent);
+};
 //Running async function
-(async ()=>{
-    //Defining Essentials
-    let movieURL='https://www.imdb.com/title/tt4154796/?ref_=nv_sr_srsg_0'
-    let browser= await puppeteer.launch({headless:true});
-    let page=await browser.newPage();
-    //Scarpping Data 
-    await page.goto(movieURL,{waitUntil:'networkidle2'});
-    let data = await page.evaluate(()=>{
-        let movieName=document.querySelector('div[class="title_wrapper"]> h1').innerText
-        let rating=document.querySelector('span[itemprop="ratingValue"]').innerText
-        let plot=document.querySelector('div[class="summary_text"] ').innerText 
-        
-        return{
-            movieName,
-            rating,
-            plot
-        }
-    });
-//Logging Data
-console.log(data)
-debugger;
-await browser.close();
-}   
-)();
+(async () => {
+  //Defining Essentials
+  let movieURL =
+    "https://m.imdb.com/what-to-watch/fan-favorites/?ref_=watch_tpks_tab";
+  let browser = await puppeteer.launch({
+    args: ["--disable-web-security"],
+    headless: false,
+  });
+  let page = await browser.newPage();
+  await preparePageForTests(page);
+  //Scarpping Data
+  await page.goto(movieURL, { waitUntil: "networkidle0" });
+  const images = await page.$$eval(".ipc-image", (node) =>
+    node.map((n) => n.src)
+  );
+  const names = await page.$$eval(
+    'a[class="ipc-poster-card__title ipc-poster-card__title--clamp-2 ipc-poster-card__title--clickable"]>span',
+    (node) => node.map((n) => n.innerText)
+  );
+  const rating = await page.$$eval(
+    'span[class="ipc-rating-star ipc-rating-star--baseAlt ipc-rating-star--imdb"]',
+    (node) => node.map((n) => n.innerText)
+  );
+
+  let data = {
+    names: names,
+    images: images,
+    rating: rating,
+  };
+  console.log(data);
+  debugger;
+  await browser.close();
+})();
